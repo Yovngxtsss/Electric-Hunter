7aedc1c524f8633d43150919bd6bb6ffe17ea438
Yunai <yhj0001@alu.medac.es>
2025-11-19 14:23:52 +0100

    Revert "Commit yunai"
    This reverts commit 7b30c1b9de37d27a5221bf0a2efbfb6ee28e028e.

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'electrichunter' );
+define( 'DB_NAME', 'Electric Hunter' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
